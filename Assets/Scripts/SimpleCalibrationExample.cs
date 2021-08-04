c00de43be4164df0cff3bf4e6a33e8a783bf513c
doJester13 <minarini.work@gmail.com>
2021-08-04 16:54:19 +0200

    next mind materials test

---
+++ Assets/Scripts/SimpleCalibrationExample.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using NextMind;
+using NextMind.Calibration;
+using UnityEngine.UI;
+using NextMind.Devices;
+
+public class SimpleCalibrationExample : MonoBehaviour{
+    public CalibrationManager calibrationManager;
+    public Text resultsText;
+    
+    void Start(){
+        StartCoroutine(startCalibrationWhenReady());
+    }
+
+    private IEnumerator startCalibrationWhenReady(){
+        yield return new WaitUntil(NeuroManager.Instance.IsReady);
+        calibrationManager.onCalibrationResultsAvailable.AddListener(onReceivedResults);
+        calibrationManager.StartCalibration();
+    }
+
+    private void onReceivedResults(Device device, CalibrationResults.CalibrationGrade grade){
+        resultsText.text = $"Received results for {device.Name} with a grade of {grade}";
+    }
+}
