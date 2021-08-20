72a9e51fbe377c48e105652e774385e053e5f0e0
Matteo Baraldi <matteo@tntc.it>
2021-08-20 19:47:12 +0200

    wip logic

---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    public Transform target;
+    public Animator animator;
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space))
+            animator.SetTrigger("hit");
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Spell")) {
+            animator.SetTrigger("hit");
+        }
+    }
+}
