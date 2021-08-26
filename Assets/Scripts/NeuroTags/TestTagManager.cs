bfb10e50fd470dbddd193cbad29f6ebc38c3c849
doJester13 <minarini.work@gmail.com>
2021-08-26 17:08:54 +0200

    tag feedback and precision

---
+++ Assets/Scripts/NeuroTags/TestTagManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using NextMind;
+using NextMind.Devices;
+using NextMind.Calibration;
+using System.Collections.Generic;
+
+public class TestTagManager : MonoBehaviour{
+    public CalibrationManager calibrationManager;
+    public Text resultsText;
+    public List<GameObject> objectToUse;
+    
+    void Start(){
+        StartCoroutine(startCalibrationWhenReady());
+    }
+
+    IEnumerator startCalibrationWhenReady(){
+        yield return new WaitUntil(NeuroManager.Instance.IsReady);
+        calibrationManager.onCalibrationResultsAvailable.AddListener(onReceivedResults);
+        calibrationManager.StartCalibration();
+        resultsText.text = "";
+    }
+
+    void onReceivedResults(Device device, CalibrationResults.CalibrationGrade grade){
+        resultsText.gameObject.SetActive(true);
+        resultsText.text = $"Configuration complete! Received results for {device.Name} with a grade of {grade}";
+        StartCoroutine(closeTextAndStartTest());
+    }
+
+    IEnumerator closeTextAndStartTest(){
+        yield return new WaitForSeconds(3);
+        resultsText.gameObject.SetActive(false);
+        activateAll();
+    }
+
+    void activateAll(){
+        objectToUse.ForEach( x => x.SetActive(true));
+    }
+}
