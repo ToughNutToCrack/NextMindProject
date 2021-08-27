8be6ea234abcafa79e1bed2e827e7b3ad5d6f134
doJester13 <minarini.work@gmail.com>
2021-08-27 18:44:47 +0200

    throwing logic wip

--- Assets/Scripts/HandManager.cs
+++ Assets/Scripts/HandManager.cs
@@ -1,45 +1,88 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class HandManager : MonoBehaviour
-{
-    public float distanceThreshold = 0.2f;
-    public int numberOfStep = 2;
-    Vector3 oldTransform;
-    int counter;
-    public SpellManager spellManager;
+public class HandManager : MonoBehaviour{
+    // public float distanceThreshold = 0.2f;
+    // public int numberOfStep = 2;
+    // Vector3 oldTransform;
+    // int counter;
+    // public SpellManager spellManager;
+    // public OVRHand hand;
+
+    // void Start(){
+    //     oldTransform = transform.position;
+    // }
+
+    // void Update(){
+    //     if (!hand.IsTracked){
+    //         counter = 0;
+    //         return;
+    //     }
+
+    //     var distance = Vector3.Distance(transform.position, oldTransform);
+
+    //     if (distance > distanceThreshold){
+    //         counter++;
+    //         if (counter >= numberOfStep)
+    //             spellManager.ThrowSpell(transform.position - oldTransform);
+    //     }else{
+    //         counter = 0;
+    //     } 
+
+    //     oldTransform = transform.position;
+    // }
+
+    public Transform head;
     public OVRHand hand;
-    // List<Vector3> positions;
+    public Transform spellSpwanPoint;
+    [Header("Debug")]
+    public GameObject spellPrefab;
+    public VRButtonListener buttonListener;
 
+    GameObject currentSpell = null;
+    Vector3 previousPosition;
+    Vector3 currentVelocity;
 
-
-    void Start()
-    {
-        // positions = new List<Vector3>();
-        oldTransform = transform.position;
+    void Start() {
+        previousPosition = transform.position;    
     }
 
-    void Update()
-    {
-        if (!hand.IsTracked)
-        {
-            counter = 0;
-            return;
+    bool openHand() => hand.IsSystemGestureInProgress;
+
+    void Update(){
+        if(buttonListener.isGameStarted){
+            if(openHand() && currentSpell == null){
+                currentSpell = Instantiate(spellPrefab, spellSpwanPoint);
+            }
         }
 
-        var distance = Vector3.Distance(transform.position, oldTransform);
-
-        if (distance > distanceThreshold)
-        {
-            counter++;
-            if (counter >= numberOfStep)
-                spellManager.ThrowSpell(transform.position - oldTransform);
+        if(currentSpell != null){
+            currentVelocity = calculateCurrentVelocity();
+            // print(currentVelocity);
+            // printDistanceFromHead();
         }
-        else
-        {
-            counter = 0;
-        } 
-
-        oldTransform = transform.position;
     }
+
+    Vector3 calculateCurrentVelocity(){
+        Vector3 velocity = (transform.position - previousPosition) / Time.deltaTime;
+        previousPosition = transform.position;
+        return velocity;
+    }
+
+    void printDistanceFromHead(){
+        var heading = currentSpell.transform.position - head.position;
+        var distance = heading.magnitude;
+        print(distance);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "SpellBoxController" && currentSpell != null){
+            currentSpell.transform.SetParent(null);
+            Rigidbody rb = currentSpell.AddComponent<Rigidbody>();
+            print(currentVelocity);
+            rb.AddForce(currentVelocity * 500);
+            currentSpell = null;
+        }
+    }
+
 }
---
+++ Assets/Scripts/VRButtonListener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VRButtonListener : MonoBehaviour{
+
+    public bool isGameStarted = false;
+
+    void OnTriggerEnter(Collider other) {
+        OVRHand hand = other.GetComponent<OVRHand>();
+        if(hand != null){
+            isGameStarted = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
