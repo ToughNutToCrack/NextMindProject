21bf692a1a8e5b064dd78e685a461f582810a203
doJester13 <minarini.work@gmail.com>
2021-09-03 17:42:12 +0200

    fire impact

--- Assets/Scripts/Impact.cs
+++ Assets/Scripts/Impact.cs
@@ -4,27 +4,33 @@ using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
 public class Impact : MonoBehaviour{
+    const string FADE = "_Fade";
     public DecalProjector decal;
     public float speed = 1;
+    public float timeBeforeDecal = 0;
     public float timeBeforeFade = 2;
 
     Material currentMaterial;
     float elapsedTime = 0;
 
     void Start() {
-        Destroy(gameObject, timeBeforeFade * 2);
+        Destroy(gameObject, timeBeforeDecal + timeBeforeFade * 2);
         Material mat = new Material(decal.material);
         decal.material = mat;
         currentMaterial = decal.material;
+        currentMaterial.SetFloat(FADE, 1);
     }
 
     void Update(){
         elapsedTime += Time.deltaTime;
-        if(elapsedTime >= timeBeforeFade){
-            var fadeValue = currentMaterial.GetFloat("_Fade");
+        if(elapsedTime > timeBeforeDecal && elapsedTime < timeBeforeDecal + timeBeforeFade){
+            currentMaterial.SetFloat(FADE, .1f);
+        }
+        if(elapsedTime >= timeBeforeDecal + timeBeforeFade){
+            var fadeValue = currentMaterial.GetFloat(FADE);
             if(fadeValue <= 1){
                 fadeValue += Time.deltaTime * speed;
-                currentMaterial.SetFloat("_Fade", fadeValue);
+                currentMaterial.SetFloat(FADE, fadeValue);
             }
         }
     }
